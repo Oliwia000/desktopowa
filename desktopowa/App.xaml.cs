a9070821ed770e8f2dd7010b280b093b70ff9ff4
teren <teren@KOMP>
2025-09-23 16:26:00 +0200

    Dodaj pliki projektów.

---
+++ desktopowa/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace desktopowa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ desktopowa/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace desktopowa
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
