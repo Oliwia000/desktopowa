70bc049f575f82d9be83b7706e77f52e65c41af1
teren <teren@KOMP>
2025-09-23 16:47:15 +0200

    poprawki

--- desktopowa/MainPage.xaml.cs
+++ desktopowa/MainPage.xaml.cs
@@ -3,31 +3,26 @@
 public partial class MainPage : ContentPage
 {
     int r = 255, g = 255, b = 255;
-
     public MainPage()
     {
         InitializeComponent();
         UpdateBigBox();
     }
-
     private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
     {
         r = (int)SliderR.Value;
         g = (int)SliderG.Value;
         b = (int)SliderB.Value;
-
         LabelR.Text = r.ToString();
         LabelG.Text = g.ToString();
         LabelB.Text = b.ToString();
 
         UpdateBigBox();
     }
-
     private void UpdateBigBox()
     {
         BigBox.Color = Color.FromRgb(r, g, b);
     }
-
     private void OnPobierzClicked(object sender, EventArgs e)
     {
         SmallBox.Color = Color.FromRgb(r, g, b);
